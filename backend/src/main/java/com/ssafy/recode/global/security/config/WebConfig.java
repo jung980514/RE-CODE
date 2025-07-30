a05ce5a89ea3765db9a6815383cc788f34c1235d
정지용 <jung980514@naver.com>
2025-07-30 16:53:22 +0900

    #fix:Swagger CORS 오류 수정

--- backend/src/main/java/com/ssafy/recode/global/security/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/recode/global/security/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") // 모든 Origin 허용
-                .allowedMethods("GET","POST","PUT","DELETE") // 주요 HTTP 메서드 허용
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS") // 주요 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 요청 헤더 허용
                 .allowCredentials(false); // 자격 증명 포함 안 함
 
