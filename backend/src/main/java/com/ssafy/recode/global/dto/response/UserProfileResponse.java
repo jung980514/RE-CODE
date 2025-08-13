de1448cf4f246b38c3a0c96d0020a4fff9500317
dudals7142 <dudals7142@naver.com>
2025-08-13 10:50:03 +0900

    #fix:회원 - 마이페이지 DTO추가

---
+++ backend/src/main/java/com/ssafy/recode/global/dto/response/UserProfileResponse.java
@@ -0,0 +1,28 @@
+package com.ssafy.recode.global.dto.response;
+
+import com.ssafy.recode.domain.auth.entity.User;
+import com.ssafy.recode.global.enums.Role;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+public class UserProfileResponse {
+
+    private final Long id;
+    private final String email;
+    private final String name;
+    private final LocalDate birthDate;
+    private final String phone;
+    private final Role role;
+    private final String profileImageUrl; // Presigned URL을 담을 필드
+
+    public UserProfileResponse(User user, String presignedUrl) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.birthDate = user.getBirthDate();
+        this.phone = user.getPhone();
+        this.role = user.getRole();
+        this.profileImageUrl = presignedUrl;
+    }
+}
